1e9d7227c4d514208cd935a0e6952d7ea1b2785f
Raman Magar <95367709+Ramanmagar@users.noreply.github.com>
2023-01-04 13:42:57 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,7 @@ import cv2
 import numpy as np
 import streamlit as st
 from tensorflow.keras.models import load_model
+from streamlit_webrtc import webrtc_streamer
 from tensorflow.keras.preprocessing.image import img_to_array
 from streamlit_webrtc import (
     ClientSettings,
