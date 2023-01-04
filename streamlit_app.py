0542f09a739f96f77330e91866e01121a3955b92
Raman Magar <95367709+Ramanmagar@users.noreply.github.com>
2023-01-04 14:04:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -58,10 +58,10 @@ class VideoTransformer(VideoTransformerBase):
 webrtc_streamer(
         key="emotion-detection",
         mode=WebRtcMode.SENDRECV,
-        #client_settings=WEBRTC_CLIENT_SETTINGS,
+        client_settings=WEBRTC_CLIENT_SETTINGS,
     
-        rtc_configuration=WEBRTC_CLIENT_SETTINGS,
-        media_stream_constraints=WEBRTC_CLIENT_SETTINGS,
+        #rtc_configuration=WEBRTC_CLIENT_SETTINGS,
+        #media_stream_constraints=WEBRTC_CLIENT_SETTINGS,
         #video_transformer_factory=VideoTransformer,
     
         video_processor_factory=VideoTransformer,
